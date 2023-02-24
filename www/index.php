d11cf29d836417844810184b088e6602fb839d6e
Dmitry <metaldr@yandex.ru>
2023-02-24 17:55:43 +0300

    Циклы обработки + опция не затирать source

--- www/index.php
+++ www/index.php
@@ -4,7 +4,10 @@ $MAX_SIZE = 1024;
 $images = array_slice(scandir(__DIR__ . '/source'), 2);
 $signIM = new Imagick(__DIR__ . '/sign.png');
 $waterMarkIM = new Imagick(__DIR__ . '/watermark40.png');
-clearFolder('target');
+
+if (!isset($_GET['preserve'])) {
+    clearFolder('target');
+}
 
 foreach($images as $imageName){
     $imageIM = new Imagick();
@@ -16,15 +19,22 @@ foreach($images as $imageName){
     }
 
     if (isset($_GET['full'])) {
+        $iterationsMax = $_GET['full'] ?? 1;
         $waterMarkIM->resizeImage($MAX_SIZE, $MAX_SIZE, IMagick::FILTER_LANCZOS, 0);
-        $imageIM->compositeImage($waterMarkIM, IMagick::COMPOSITE_OVER, $waterMarkX, 0);
-        $imageIM->compositeImage($waterMarkIM, IMagick::COMPOSITE_OVER, $waterMarkX, $MAX_SIZE);
+
+        for($i = 0; $i < $iterationsMax; $i++){
+            $imageIM->compositeImage($waterMarkIM, IMagick::COMPOSITE_OVER, $waterMarkX, 0);
+            $imageIM->compositeImage($waterMarkIM, IMagick::COMPOSITE_OVER, $waterMarkX, $MAX_SIZE);
+        }
     }
 
     $imageIM->resizeImage($MAX_SIZE, $MAX_SIZE, IMagick::FILTER_LANCZOS, 0, true);
     $imageIM->writeImage(__DIR__ . DIRECTORY_SEPARATOR . 'target' . DIRECTORY_SEPARATOR . 'wat_' . $imageName);
 }
-clearFolder('source');
+
+if (!isset($_GET['preserve'])) {
+    clearFolder('source');
+}
 
 
 if (empty($images)) {
